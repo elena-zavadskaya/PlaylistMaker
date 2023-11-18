d078c3cdb83eed9b2369c9aad1932cfeb3a0ae72
elena-zavadskaya <ele.zavadskaya.na@gmail.com>
2023-11-18 21:56:45 +0300

    Исправила кнопку назад и некоторые мелкие нюансы.

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -3,14 +3,14 @@ package com.practicum.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backButton = findViewById<Button>(R.id.back_button)
+        val backButton = findViewById<ImageView>(R.id.back_button)
 
         backButton.setOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
